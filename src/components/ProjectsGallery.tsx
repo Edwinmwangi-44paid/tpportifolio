6ffc69bef6e8dfa04347eb6e08eb58996f393d37
edwinmwangi-44paid <cheedu66@gmail.com>
2025-05-15 08:54:51 +0000

    7th

--- src/components/ProjectsGallery.tsx
+++ src/components/ProjectsGallery.tsx
@@ -42,7 +42,7 @@ const ProjectsGallery = ({
           Explore my recent work and the technologies I've been working with.
         </p>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-2 sm:px-0">
           {projects.map((project) => (
             <ProjectCard
               key={project.id}
